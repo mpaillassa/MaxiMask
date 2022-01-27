cf85c2c3132719de6752fc324cf05bc588fa6c51
mpaillassa <mpaillassa@gmail.com>
2022-01-27 11:51:13 +0100

    going to 1.3.3 as 1.3.2 upload failed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MaxiMask",
-    version="1.3.2",
+    version="1.3.3",
     author="Maxime Paillassa",
     author_email="maxime.paillassa@nagoya-u.jp",
     description="Convolutional neural networks to detect contaminants in astronomical images.",
